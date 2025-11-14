fb97c93c512ed2dae3b632b084150ed34e9d350a
AdriMarchena24 <adriana.marchena@zagitas.com>
2025-11-14 12:26:24 -0500

    Se actualiza enviroment prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+export const environment = {
+  production: true,
+  azure: {
+    clientId: '73985f8f-99e5-4a32-8899-c15bc727a982',
+    tenantId: '4b4bc9af-c684-4b35-bcdd-c526e7b409f0',
+    redirectUri: 'https://soporte-tickets.azurewebsites.net/auth-redirect'
+  },
+  api: {
+    tickets: {
+      list: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8f49b05e4c464caead53380d4205f98e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_oPeGN7fmsmb6BWWwOh16gUu8jriWvxO6u2mVhXQAKo',
+      create: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/b137fb58589947708d9e171c9082f014/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=S2VEnTP1b1AHBXCRSvS0G4bk4V1w7sUdzYEtUJw6TXk',
+      attachments: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/beae49bcccb14626bb81b2bd0405be3c/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nAfl9pqTWKkE24AmO0mjIk1HB1I4FQBXHBpnF9hJp5k',
+      detail: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/20d2c709953a4ebabee52c1e95e87625/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6BfKjThKmnk_2dAbDbdZHn7I0PSOmhL5jSoiPAOwtSU',
+      update: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/a470b84ffaa64f48ad6685f80a171aa2/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=rK6PL6Dd6tOzjA8MafaNAtSckIZz5S4OKfdYSjgzVpI', 
+      delete: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/90a621d4f7c0418897d7ee409e54d164/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LDc2S6DGxIX0FJL5yzI1SPsPmyRIq5FOxGvaSaUTQs4', 
+    },
+    users: {
+      rol: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/aa96d24f29f148678694059211d2f2c8/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=h4a4FFl3lqtR56JzMfWwKtCTACx9-LaMB2BTlKFv7xw'
+    },
+    options: {
+      projects: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/368a1ce60d57476281de289e39f89ed8/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=saPDuT413Dvo5HPpgFMQ-eiMALYV8ihp8VFKVaSESNM',
+      status: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/c33386dbec7243ca82babbc6e52232cd/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=sqkkveYfp3TbXBquKNEio4LuiXqxFghNhFyhkoWMGLs',
+      users: 'https://c5ddbf3c88afef3a874075a39aeefd.ed.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/770d9595002842a5b07923f9aa70d6dc/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kwaygewLJ1w0fUGM_LYu7UHU_QtQqVXtMQQgQ8IZGGc'
+    }
+  }
+};
